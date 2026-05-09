9365c66fbd5e71cb7524be70e3ccb47695621d9c
IzPaul <junpaularradaza9@gmail.com>
2026-05-09 17:10:02 +0800

    Fixed profile page bugs

---
+++ Backend/Riderent/src/main/java/com/example/Riderent/features/vehicles/Vehicle.java
@@ -0,0 +1,112 @@
+package com.example.Riderent.features.vehicles;
+
+
+
+@Entity
+public class Vehicles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+
+    private String type;
+
+    private Double rating;
+
+    @Embedded
+    private Address address;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model, String type, Double rating, Address address) {
+        this.model = model;
+        this.type = type;
+        this.rating = rating;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
+
+@Embeddable
+class Address {
+
+    @Column(name = "region")
+    private String region;
+
+    @Column(name = "province")
+    private String province;
+
+    @Column(name = "city")
+    private String city;
+
+    public Address() {
+    }
+
+    public Address(String region, String province, String city) {
+        this.region = region;
+        this.province = province;
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
