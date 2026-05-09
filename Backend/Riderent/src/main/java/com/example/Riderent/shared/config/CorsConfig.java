d98de09b3050bc1e59fd73ba77e9d7c41c9a7122
IzPaul <junpaularradaza9@gmail.com>
2026-05-09 19:25:50 +0800

    added website URL to CORS

--- Backend/Riderent/src/main/java/com/example/Riderent/shared/config/CorsConfig.java
+++ Backend/Riderent/src/main/java/com/example/Riderent/shared/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:3000",
-                                "https://riderent.onrender.com"
+                                "https://riderent.onrender.com",
+                                "https://riderent-website.onrender.com"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*");
